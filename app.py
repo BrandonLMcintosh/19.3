738a2cbb27715bb7eaca126c25b835674a4f4155
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-12-14 15:41:23 -0800

    porting from home PC

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys
 
@@ -8,19 +8,25 @@ debug = DebugToolbarExtension(app)
 
 responses = {}
 current_survey = surveys["satisfaction"]
+current_question = 0
 
 @app.route("/")
 def index():
-    print(current_survey)
     return render_template("index.html", survey=current_survey)
 
 @app.route("/question/<num>")
 def question(num):
-    current_question = num
-    return render_template("question.html", survey=current_survey, num=int(num))
+    global current_question
+    num = int(num)
+    if num != current_question:
+        num = current_question
+        return redirect(f"/question/{num}")
+    return render_template("question.html", survey=current_survey, num=num)
 
 @app.route("/answers/<num>", methods=["POST"])
 def answer(num):
+    global current_question
+    current_question += 1
     question = current_survey.questions[int(num)].question
     answer = request.form[question]
     responses[question] = answer
